d27e2a48e1d62143f8db30e2796b3645a5b2f5c6
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-06-01 17:44:01 -0300

    Creates class responsible for transforming Hash into Link

---
+++ lib/redu/link_parser.rb
@@ -0,0 +1,11 @@
+module Redu
+  class LinkParser
+    def initialize(links)
+      @raw_links = links
+    end
+
+    def parse
+      @parsed_links ||= @raw_links.map { |link| Link.new(link) }
+    end
+  end
+end
